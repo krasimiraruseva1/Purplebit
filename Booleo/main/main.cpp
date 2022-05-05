eb0959d5df375b63068e3371da3460d6ef267c40
VZKardzheva20 <VZKardzheva20@codingburgas.bg>
2022-05-05 22:56:22 +0300

    Add render window functions

--- Booleo/main/main.cpp
+++ Booleo/main/main.cpp
@@ -13,6 +13,8 @@ int main(int argc, char* args[])
 	}
 
 	RenderWindow window("Booleo", 1280, 720);
+	SDL_Texture* mainMenu = window.loadTexture(""); //main menu screen here
+
 	bool gameRunning = true;
 	SDL_Event event;
 
@@ -23,8 +25,10 @@ int main(int argc, char* args[])
 			if (event.type == SDL_QUIT)
 				gameRunning = false;
 		}
+		window.clear();
+		window.render(mainMenu);
+		window.display();
 	}
-
 	window.cleanUp();
 	SDL_Quit();
 
--- Booleo/main/renderWindow.cpp
+++ Booleo/main/renderWindow.cpp
@@ -13,7 +13,32 @@ RenderWindow::RenderWindow(const char* p_title, int p_w, int p_h)
 	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
+SDL_Texture* RenderWindow::loadTexture(const char* p_filePath)
+{
+	SDL_Texture* texture = NULL;
+	texture = IMG_LoadTexture(renderer, p_filePath);
+	if (texture == NULL)
+	{
+		std::cout << "Failed to load texture. Error: " << SDL_GetError() << std::endl;
+	}
+	return texture;
+}
 void RenderWindow::cleanUp()
 {
 	SDL_DestroyWindow(window);
+}
+
+void RenderWindow::clear()
+{
+	SDL_RenderClear(renderer);
+}
+
+void RenderWindow::render(SDL_Texture* p_tex)
+{
+	SDL_RenderCopy(renderer, p_tex, NULL, NULL);
+}
+
+void RenderWindow::display()
+{
+	SDL_RenderPresent(renderer);
 }
