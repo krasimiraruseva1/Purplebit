50ad2475ad3803be4e932f04a5d5076feff52154
unknown <ERKeserdzhieva20@codingburgas.bg>
2022-05-12 02:25:10 +0300

    Remove unnecessary comments

--- Booleo/main/main.cpp
+++ Booleo/main/main.cpp
@@ -93,29 +93,7 @@ int main(int argc, char* args[])
 	float accumulator = 0.0;
 	float currentTime = utils::hireTimeInSeconds();
 	
-	/*int deck[] = {0, 1, 2, 3, 4, 5, 6, 7, 8,
-				  9, 10, 11, 12, 13, 14, 15,
-				  16, 17, 18, 19, 20, 21, 22,
-				  23, 24, 25, 26, 27, 28, 29,
-				  30, 31, 32, 33, 34, 35, 36,
-				  37, 38, 39, 40, 41, 42, 43,
-				  44, 45, 46, 47 };
-	for (int i = 0; i < 47; i++)
-	{
-		gameplay.shuffle(&deck[i], 48);
-	}
-
-	for (int i = 4; i < 8; i++)
-
-	{
-		P2hand[i] = deck[i];
-		for (int j = i; j <= 48; j++)
-		{
-			deck[i] = deck[i + 1];
-			deck[48 - 1] = 0;
-		}
-		std::cout << P2hand[i] << " ";
-	}*/
+	
 	while (gameRunning)
 	{
 		int startTicks = SDL_GetTicks();
@@ -417,19 +395,6 @@ int main(int argc, char* args[])
 				window.render(PvCnotButton);
 			}
 			
-			/*ENTITY* playCards[6];
-			int* cards = gameplay.GetCards();
-
-			for (int i = 0; i < 6; i++)
-			{
-				if (cards[i]) {
-					playCards[i] = new ENTITY(i * 50, 0, BackCard);
-				}
-				else {
-					playCards[i] = new ENTITY(i * 50, 0, cards);
-				}
-				window.render(*playCards[i]);
-			}*/
 		}
 
 
